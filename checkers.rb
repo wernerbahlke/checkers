276798b3f9fefae7395e649b43fe77f203947dae
Werner Bahlke <werner.bahlke@gmail.com>
2011-07-02 16:24:41 -0700

    Bring back checkers

--- checkers.rb
+++ checkers.rb
@@ -1,3 +1,2 @@
 class Checkers
-
 end
--- t_checkers_spec.rb
+++ t_checkers_spec.rb
@@ -2,7 +2,10 @@ require './checkers'
 
 describe Checkers do
   before :each do
-    @checkers = Checkers
+    @checkers = Checkers.new
   end
 
+  it "should have a setup method" do
+    @checkers.setup.should_not be_nil
+  end  
 end
