ee2f3bf9e4fcc331df5eed2f6b137f22a0dae8d1
Werner Bahlke <werner.bahlke@gmail.com>
2011-07-02 17:57:19 -0700

    Initialized board with black, null, and white

--- checkers.rb
+++ checkers.rb
@@ -1,9 +1,33 @@
 class Checkers
 
+  $BLACK = 1
+  $WHITE = 2
+  $EMPTY = 0
+
+# The board cells start at array position 0 to 31. Coordinates are from 1 to 32
+
   def initialize
     @board = Array[32]
-    for i in (0..31)
-      @board[i] = i
+    initializeTop
+    initializeMiddle
+    initializeBottom
+  end
+
+  def initializeTop
+    for i in (1..12)
+      @board[i - 1] = $BLACK
+    end
+  end
+
+  def initializeMiddle
+    for i in (13..20)
+      @board[i - 1] = $EMPTY
+    end
+  end
+
+  def initializeBottom
+    for i in (21..32)
+      @board[i - 1] = $WHITE
     end
   end
 
